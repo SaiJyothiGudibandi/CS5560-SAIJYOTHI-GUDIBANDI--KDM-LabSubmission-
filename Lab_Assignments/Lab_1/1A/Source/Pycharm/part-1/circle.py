ec09b25f2c0233d2d934aa5d9acdea700ff010d7
saijyothi9 <saijyothigudibandi9@gmail.com>
2017-06-13 11:46:08 -0500

    commit

---
+++ Lab_Assignments/Lab_1/1A/Source/Pycharm/part-1/circle.py
@@ -0,0 +1,10 @@
+import math
+radius=input('Enter Radius of the circle:') #Taking input radius from the user
+r=int(radius)
+
+A=math.pi*r*r    #Area=A
+C=2*math.pi*r    #Circumference=C
+print('Area of the circle is:',A)
+print('Circumference  of the circle is:',C)
+
+
